9a0728479f2ada35d1191c62c6f351062f89f806
Linux <erictheiler15@gmail.com>
2023-11-10 08:44:49 +0100

    Final Fix?

--- CoMa/homework_1/PA01.py
+++ CoMa/homework_1/PA01.py
@@ -1,10 +1,11 @@
-def roots(a1,a2,a3,b1,b2,b3):
-    c=[a1*b1,
-    a1*b2+a2*b1,
-    a1*b3+a2*b2+a3*b1,
-    a2*b3+a3*b2,
-    a3*b3]
+def roots(a1, a2, a3, b1, b2, b3):
+    c = [a1*b1, a1*b2+a2*b1, a1*b3+a2*b2+a3*b1, a2*b3+a3*b2, a3*b3]
 
-    changes = sum((a < 0) != (b < 0) for a,b in zip(c, c[1:]))
+    print(c, c[1:])
 
-    return("Das Polynom hat eine ungerade Anzahl von positiven reellen Nullstellen") if changes % 2 != 0 else return("Das Polynom hat eine gerade Anzahl von positiven reellen Nullstellen")
+    changes = sum((a < 0) != (b < 0) for a, b in zip(c, c[1:]))
+
+    if changes % 2 != 0:
+        return "Das Polynom hat eine ungerade Anzahl von positiven reellen Nullstellen."
+    elif changes % 2 == 0:
+        return "Das Polynom hat eine gerade Anzahl von positiven reellen Nullstellen."
