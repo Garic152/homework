664abf41359b6d93335c6927488ea8a655c5eeeb
Linux <erictheiler15@gmail.com>
2023-12-15 09:14:45 +0100

    added hashing function, ressources now get hashed

---
+++ TKN/Praxis_2/rn-praxis/hashing.c
@@ -0,0 +1,10 @@
+#include "hashing.h"
+#include <arpa/inet.h>
+#include <openssl/sha.h>
+#include <string.h>
+
+uint16_t hash(const char *str) {
+  uint8_t digest[SHA256_DIGEST_LENGTH];
+  SHA256((uint8_t *)str, strlen(str), digest);
+  return htons(*((uint16_t *)digest));
+}
---
+++ TKN/Praxis_2/rn-praxis/hashing.h
@@ -0,0 +1,8 @@
+#include <arpa/inet.h>
+
+#ifndef HASHING_H
+#define HASHING_H
+
+uint16_t hash(const char *str);
+
+#endif // HASHING_H
